a8dbdb71100e85116c8c86732e4afd324cabd29b
Tom Sedgman <t.sedgman@gmail.com>
2014-08-26 22:36:25 +0100

    system now supports comarison of two data variables, however, one is fixed until the button for clearing the chart is called. button also needs to be adjusted to work using ajax

--- FP2/web/resources/JavaScript/Graph.js
+++ FP2/web/resources/JavaScript/Graph.js
@@ -1,29 +1,47 @@
 
 google.load("visualization", "1", {packages:["corechart"]});
 google.setOnLoadCallback(drawChart());
-
     
 function drawChart() 
 {
       
-    function buildData()
+        var title = "Node: "+currentNode;
+        var xTitle = "Date";
+        var yTitle = titles[currentVariable];
+//        var dataCount = 0;
+        if (totalData !== null)
+        {
+            totalData = google.visualization.data.join(totalData,newData(),'full',[[0,0]],[1],[1]);
+//            dataCount++;
+        }
+        else
+        {
+            totalData = newData();
+//            dataCount = 1;
+        }
+        
+        var options = 
+        {
+            title: title,
+            hAxis: {title: xTitle},
+            vAxis: {title: yTitle},
+            legend: 'none',
+            pointSize: 1,
+        };
+        var chart = new google.visualization.ScatterChart(document.getElementById('graph'));
+        chart.draw(totalData, options);
+   
+   function newData()
     {
-        //var dataArray = dataIn.split("|");
         var data = new google.visualization.DataTable();
         data.addColumn('date', 'Date');
         data.addColumn('number', titles[currentVariable]);
-        var num = (dataIn.length);//dataArray.length;
+        var num = (dataIn.length);
         data.addRows(num/2);
-//        var dateFormatter = new google.visualization.DateFormat
-//            ({
-//                pattern: "dd/MM/yy-hh:mm:ss"
-//            });
-//            dateFormatter.format(dataIn,0);
         var i = 0;
         var j = 0;
         while (i<num)
         {
-            
             var d = (dataIn[i]);
             if (i%2===0)
             {
@@ -33,7 +51,6 @@ function drawChart()
             }
             else
             {
-                
                 data.setCell(j,1,parseFloat(d));                
                 i++;
                 j++;
@@ -41,19 +58,4 @@ function drawChart()
         }
         return data;
     }
-    
-        var title = "Node: "+currentNode;
-        var xTitle = "Date/RecordNumber";
-        var yTitle = titles[currentVariable];
-        var data = buildData();
-        var options = 
-        {
-            title: title,
-            hAxis: {title: xTitle},
-            vAxis: {title: yTitle},
-            legend: 'none'
-        };
-        var chart = new google.visualization.ScatterChart(document.getElementById('graph'));
-        chart.draw(data, options);
-    
 }
